15b34c75d0be3469522f0041ac2515787297e2db
Ekelemon112 <solomonnwachukwu009@gmail.com>
2023-12-02 22:03:11 +0100

    first commit

---
+++ java.js
@@ -0,0 +1,17 @@
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-130px";
+};
+
+// ================= end  =================
+
+// var content = document.getElementsByTagName('body')[0];
+// var darkmode = document.getElementById('dark-change');
+// darkmode.addEventListener('click', function(){
+//     darkmode.classList.toggle('active');
+//     content.classList.toggle('night');
+// });
